a73116a54212cf0c32ed78c3435de04a5d75cd07
Hari Houdini <hariharasudhanshaktivel@gmail.com>
2026-06-16 20:53:13 +0100

    Enhance project configuration and HTML structure
    - Added formatter configuration in biome.json to enable HTML formatting
    - Updated build.config.ts to include additional entry points and improved error logging
    - Refined index.html with new styles, structure, and a terminal window template
    - Configured VSCode settings for default HTML formatting with Biome

--- build.config.ts
+++ build.config.ts
@@ -2,23 +2,27 @@ import { htmlReplacePlugin } from 'plugins/html-placeholder-replace.plugin';
 import { propertiesLoaderPlugin } from 'plugins/properties-loader.plugin';
 
 const result = await Bun.build({
-  entrypoints: ['src/main.ts', 'src/worker.ts'],
+  entrypoints: [
+    'src/main.ts',
+    'src/worker.ts',
+    'index.html',
+    'resources/messages_en.properties',
+  ],
   outdir: 'dist',
   minify: true,
-  sourcemap: true,
+  sourcemap: process.env.NODE_ENV === 'production' ? 'none' : true,
   splitting: true,
   target: 'browser',
   format: 'esm',
   plugins: [propertiesLoaderPlugin, htmlReplacePlugin],
 });
 
+for (const error of result.logs) {
+  console.info(error.message);
+}
+
 if (!result.success) {
   console.error('Failed to build project');
-
-  for (const error of result.logs) {
-    console.error(error.message);
-  }
-
   process.exit(1);
 }
 
